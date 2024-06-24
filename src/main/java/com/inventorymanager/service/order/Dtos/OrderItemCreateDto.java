d6b7b878eee9c721a45f094488c81f318db08798
Yuan_Lu <642649909@qq.com>
2024-06-24 16:00:40 +0300

    Add validation to order request

--- src/main/java/com/inventorymanager/service/order/Dtos/OrderItemCreateDto.java
+++ src/main/java/com/inventorymanager/service/order/Dtos/OrderItemCreateDto.java
@@ -1,6 +1,9 @@
 package com.inventorymanager.service.order.Dtos;
 
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,6 +12,9 @@ import java.util.UUID;
 @Getter
 @Setter
 public class OrderItemCreateDto {
+    @NotNull
     private UUID stockId;
+    @NotNull
+    @Positive(message = "The quantity of order item must be greater than 0.")
     private int quantity;
 }
